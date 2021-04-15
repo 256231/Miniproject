fea48bf671fce64c17ec713abf2155aeec469019
Y SWATHI <82105010+256231@users.noreply.github.com>
2021-04-16 00:41:53 +0530

    commit106

--- 3_Implementation/src/header.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HEADER_H__
-#define __HEADER_H__
-#include<stdlib.h>
-enum move {UP,DOWN,LEFT,RIGHT,STOP}direction;
-enum ghost_move {UP1,DOWN1,LEFT1,RIGHT1,STOP1} ghost1_dir,ghost2_dir;
-void ghost1_direction();
-void ghost2_direction();
-#endif 
